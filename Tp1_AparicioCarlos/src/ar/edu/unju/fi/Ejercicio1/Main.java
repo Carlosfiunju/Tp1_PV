2e253e958c5ddbbae898de8387cb0dee04a7b0c2
carlo <carlo@DESKTOP-SNVC1L1>
2024-04-13 18:56:08 -0300

    Ejercicio 1 del tp 1 desarrolado

--- Tp1_AparicioCarlos/src/ar/edu/unju/fi/Ejercicio1/Main.java
+++ Tp1_AparicioCarlos/src/ar/edu/unju/fi/Ejercicio1/Main.java
@@ -1,10 +1,16 @@
 package ar.edu.unju.fi.Ejercicio1;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-
+		
+		float numero1 = 5;
+		float numero2 = 8;
+		float numero3 = 1;
+		float numero4 = 20;
+		float numero5 = 9;
+		float promedio = (numero1+numero2+numero3+numero4+numero5)/5;
+		System.out.println("El promedio es: "+promedio);
 	}
 
 }
