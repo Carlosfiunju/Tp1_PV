95a93a9f664cc4730fb51f8acc07691582487713
carlo <carlo@DESKTOP-SNVC1L1>
2024-04-14 14:40:25 -0300

    Ejercicio 3 desarrollado

---
+++ Tp1_AparicioCarlos/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio3.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num;
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Ingrese un numero: ");
+		num = scanner.nextInt();
+		if (num%2==1) {
+			System.out.println("El doble del numero es: "+num*2);
+		}else {
+			System.out.println("El triple de numero es: "+num*3);
+		}
+	}
+
+}
