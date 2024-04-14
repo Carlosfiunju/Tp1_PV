dd2f98e212a9a40105e492679862af155c32f3e7
carlo <carlo@DESKTOP-SNVC1L1>
2024-04-14 12:51:44 -0300

    Ejercicio 2 desarrollado

---
+++ Tp1_AparicioCarlos/src/ar/edu/unju/fi/ejercicio2/main/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio2.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrepais = "Argentina";
+		short edad = 30;
+		double altura = 10.7;
+		double precio = 1500.0;
+		String numtelefono = "+543885123456";
+		double numcos = 0.5;
+		System.out.println("Pais: "+ nombrepais);
+		System.out.println("Edad: "+ edad);
+		System.out.println("Altura de edificio : "+altura);
+		System.out.println("Precio: "+ precio);
+		System.out.println("Telefono: "+ numtelefono);
+		System.out.println("Coseno: "+(float) Math.cos(numcos));
+	}
+
+}
